902fc50e5df1aed1368f7cfc24fb5bf777287468
Johan Lundin <johan@bluenine.se>
2016-02-27 12:05:32 +0000

    Adding tor build

--- tor.sh
+++ tor.sh
@@ -16,24 +16,3 @@ if [ ! -e "tor-${VERSION}.tar.gz" ]; then
 fi
 echo "Using tor-${VERSION}.tar.gz"
 
-# see https://www.torproject.org/docs/verifying-signatures.html.en
-# and https://www.torproject.org/docs/signing-keys.html.en
-# up to you to set up `gpg` and add keys to your keychain
-if $VERIFYGPG; then
-	if [ ! -e "tor-${VERSION}.tar.gz.asc" ]; then
-		curl -O ${TOR_DIST_URL}tor-${VERSION}.tar.gz.asc
-	fi
-	echo "Using tor-${VERSION}.tar.gz.asc"
-	if out=$(gpg --status-fd 1 --verify "tor-${VERSION}.tar.gz.asc" "tor-${VERSION}.tar.gz" 2>/dev/null) &&
-	echo "$out" | grep -qs "^\[GNUPG:\] VALIDSIG"; then
-		echo "$out" | egrep "GOODSIG|VALIDSIG"
-		echo "Verified GPG signature for source..."
-	else
-		echo "$out" >&2
-		echo "COULD NOT VERIFY PACKAGE SIGNATURE..."
-		exit 1
-	fi
-fi
-
-OUTPUT=`pwd`/output
-tar zxf tor-${VERSION}.tar.gz
