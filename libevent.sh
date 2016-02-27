d15c7bc11d99e145f7cdfad4124c0fafeafdf4ca
Johan Lundin <johan@bluenine.se>
2016-02-27 10:58:07 +0000

    First version of libevent

--- libevent.sh
+++ libevent.sh
@@ -12,7 +12,7 @@ set -e
 if [ ! -e "libevent-${VERSION}.tar.gz" ]; then
 	echo "Downloading libevent-${VERSION}.tar.gz"
 	#curl -LO https://github.com/downloads/libevent/libevent/libevent-${VERSION}.tar.gz
-    curl -LO https://sourceforge.net/projects/levent/files/release-${VERSION}/libevent-${VERSION}.tar.gz/download
+    curl -LO http://netix.dl.sourceforge.net/project/levent/release-${VERSION}/libevent-${VERSION}.tar.gz
 fi
 echo "Using libevent-${VERSION}.tar.gz"
 
@@ -22,7 +22,7 @@ if $VERIFYGPG; then
 	if [ ! -e "${SRCDIR}/libevent-${VERSION}.tar.gz.asc" ]; then
 		#curl -LO https://github.com/downloads/libevent/libevent/libevent-${VERSION}.tar.gz.asc
 
-        curl -LO https://sourceforge.net/projects/levent/files/release-${VERSION}/libevent-${VERSION}.tar.gz.asc/download
+        curl -LO http://netix.dl.sourceforge.net/project/levent/release-${VERSION}/libevent-${VERSION}.tar.gz.asc
 	fi
 	echo "Using libevent-${VERSION}.tar.gz.asc"
 	if out=$(gpg --status-fd 1 --verify "libevent-${VERSION}.tar.gz.asc" "libevent-${VERSION}.tar.gz" 2>/dev/null) &&
