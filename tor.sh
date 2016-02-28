d570cd75a269102cc7833a4bbde440e5b5c95f38
Johan Lundin <johan@bluenine.se>
2016-02-28 17:56:49 +0000

    Reverted

--- tor.sh
+++ tor.sh
@@ -9,6 +9,7 @@ TOR_DIST_URL="https://dist.torproject.org/"
 
 set -e
 
+# Download and verify tor source code
 if [ ! -e "tor-${VERSION}.tar.gz" ]; then
 	echo "Downloading tor-${VERSION}.tar.gz"
 	#curl -O https://archive.torproject.org/tor-package-archive/tor-${VERSION}.tar.gz
@@ -31,21 +32,24 @@ if $VERIFYGPG; then
 	fi
 fi
 
+# Unpack sources
 OUTPUT=`pwd`/output
 tar zxf tor-${VERSION}.tar.gz
-
 cd tor-${VERSION}
 
 # Compile fixes for Android
 sed -i.old '1s;^;#include <sys/limits.h>\n;' src/ext/trunnel/trunnel.c
 sed -i 's/pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC)/0/g' src/common/compat_pthreads.c
 
+# Setup compile flags for makefile
 export CFLAGS="$CFLAGS --sysroot=$SYSROOT -O2 -Isrc/common -I../output/openssl/include -I$OUTPUT/libevent/include/event2"
 export CPPFLAGS="$CPPFLAGS --sysroot=$SYSROOT -Isrc/common -I../output/openssl/include -I$OUTPUT/libevent/include/event2"
 
+# Remove curves causing errors
 sed -ie "s/tor_cv_can_use_curve25519_donna_c64=cross/tor_cv_can_use_curve25519_donna_c64=no/g" configure
 sed -ie "s/tor_cv_can_use_curve25519_donna_c64=yes/tor_cv_can_use_curve25519_donna_c64=no/g" configure
 
+# Configure build
 ./configure --with-openssl-dir="$OUTPUT/openssl/" \
 --with-libevent-dir="$OUTPUT/libevent" \
 --disable-asciidoc --disable-transparent \
@@ -53,6 +57,11 @@ sed -ie "s/tor_cv_can_use_curve25519_donna_c64=yes/tor_cv_can_use_curve25519_don
 
 export LDFLAGS="$LDFLAGS -arch armv7 -lz -lcrypto -levent -lssl -L$OUTPUT/openssl/lib -L$OUTPUT/libevent"
 
+# Build tor
 make -j4
 
-cp src/or/tor ../../built/
+# Distribute artefacts
+mkdir -p ../../built
+cp src/or/tor ../../built
+cp ../output/openssl/lib/libssl.so.1.0.0 ../../built
+cp ../output/openssl/lib/libcrypto.so.1.0.0 ../../built
